50e2f2b7bc30f9893e1955874c2c974f59caaf6e
gpseema <gp.seema@gmail.com>
2017-04-18 16:16:08 +0200

    Update Priming2017_TrainingPhase.m

--- Priming2017_TrainingPhase.m
+++ Priming2017_TrainingPhase.m
@@ -11,18 +11,25 @@ disp('MAIN');
 Settings.Vpn = input('Participant: ');
 
 Settings.StimSize = 350;
+Settings.SquareSize = round(Settings.StimSize/50);
+Settings.FrameSize= Settings.StimSize+2*Settings.SquareSize;
+frame_im = ones(Settings.FrameSize);
+
 Settings.StimDuration = 200;
 Settings.Fixation.Size  = 10;
-Settings.Fixation.Duration = 1000;
+Settings.Fixation.DurationRed = 1000;
+Settings.Fixation.DurationGreen = 1000;
+Settings.Feedback.Duration = 500;
 Settings.ITI = 1000;
 
-Settings.Instruction =  1; % stimulus-response mapping (0,1)    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 
+Settings.Mapping =  1; % stimulus-response mapping (0,1)    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 
 
 Settings.Background.Greyvalue = 128;
 Settings.Writing.Colour = 60;
 
 Settings.Black = 0 ;
 Settings.White = 255;
+Settings.Red = [255 0 0];
 Settings.Grey= [0 0 255];
 
 Settings.Fixation.Colour = [255 0 0];
@@ -32,12 +39,27 @@ Settings.FontSize= 12;
 
 Settings.rootdir = 'S:\AG\AG-Hesselmann-XT-Home\hesselmg\BACKUP31102011\BERLIN 2011\Studenten 2011-2013\Seema\Source Code'; % adjust rootdir
 
+KbName('UnifyKeyNames');
+
 EscapeKey = 27;
-StartKey = 32;
+SpaceKey = 32;
 ReturnKey = 13;
 
-Settings.Key1 = 37; % left
-Settings.Key2 = 40; % down
+Settings.Key1 = KbName('a'); 
+Settings.Key2 = KbName('l');
+
+% little script for getting key codes    
+% FlushEvents;  RestrictKeysForKbCheck([]);
+% WaitSecs(0.5); mykey = 1;
+% while mykey
+%     [keyIsDown, secs, keyCode] = KbCheck;
+%     if keyIsDown
+%         mykey = 0;
+%         disp(min(find(keyCode)));
+%         break;
+%     end
+%     WaitSecs(.01); %It is a good habit not to poll as fast as possible
+% end
 
 size_xy = 612; % 612/462 old experiment
 
@@ -45,7 +67,14 @@ size_xy = 612; % 612/462 old experiment
 
 % design specification
 
-Settings.numberoftrials = 5;
+Settings.numberoftrials = 128;
+
+mymapping = zeros(1,108);
+mycatchtrials = randperm(108);
+mycatchtrials = mycatchtrials(1:12);
+mymapping(mycatchtrials) = 1;
+
+Settings.CatchTrial = [zeros(1,20) mymapping]; % the first 20 trials are "normal" trials (1) with the correct stim-resp mapping, the next 108 trials contain 12 catch trials (0)
 
 %% START
 
@@ -60,7 +89,6 @@ try
     KbName('UnifyKeyNames');
     escapeKey = KbName('ESCAPE');
     
-
     screens=Screen('Screens');
     screenNumber=max(screens);
 
@@ -90,12 +118,14 @@ try
     %% Display first message and get screen details
     Screen('TextFont',w, 'Arial');
     Screen('TextSize',w, Settings.FontSize);
-    [nx, ny, bbox] = DrawFormattedText(w, 'Please wait while stimuli \n\n \n\nare being created.', x/4 , Settings.Stimuli.CenterXY(2) , Settings.Writing.Colour);
+    [nx, ny, bbox] = DrawFormattedText(w, 'Please wait...', x/2 , Settings.Stimuli.CenterXY(2) , Settings.Writing.Colour);
     Screen('Flip',w);
 
-    % load the two "effects"
+    FrameTexture=Screen('MakeTexture', w, frame_im*255, 0, 0, 0, 0, 0);
     
-    % read 2 shined images
+    % load the two "effects" stimuli
+    
+    % read 2 shined images (dummy "effects")
     MyTextures = cell(40,1);
     for i = 1:2
         MyTextures{i}=Screen('MakeTexture', w, imread(fullfile(Settings.rootdir,'images',['sf30grey_new' num2str(i) '.tif'])), 0, 0, 0, 0, 0);
@@ -103,8 +133,7 @@ try
         
     %% make empty grey texture for contrast modulation of CFS masks (and
     %% the no prime condition too)
-    grey_wo_frame = ones(Settings.StimSize)*215;
-         
+    grey_wo_frame = ones(Settings.StimSize)*215;         
     GreyTexture=Screen('MakeTexture', w, grey_wo_frame, 0, 4, 0, 0, 0);
 
     %% Hz Message
@@ -112,7 +141,7 @@ try
 
     Screen('Flip',w);
     WaitSecs(0.5);    
-    FlushEvents;  RestrictKeysForKbCheck([StartKey]);
+    FlushEvents;  RestrictKeysForKbCheck([SpaceKey]);
     while 1
         [keyIsDown, secs, keyCode] = KbCheck;
         if keyIsDown
@@ -173,9 +202,21 @@ try
 
 mytrialnum = mytrialnum + 1;
 
-% display question mark on grey background
+% display red fixation on grey background
 
 Screen('FillRect', w, Settings.Background.Greyvalue, wRect);
+Screen('DrawTexture', w, FrameTexture,[], recter(Settings.FrameSize, [xpos ypos], [x y]), [], [], 1);
+Screen('DrawTexture', w, GreyTexture,[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
+makefix_single(xpos,ypos,Settings.Fixation.Size,Settings.Fixation.Colour,w);  
+Screen('Flip',w);
+
+WaitSecs(Settings.Fixation.DurationRed/1000);
+
+% display green fixation on grey background
+
+Screen('FillRect', w, Settings.Background.Greyvalue, wRect);
+Screen('DrawTexture', w, FrameTexture,[], recter(Settings.FrameSize, [xpos ypos], [x y]), [], [], 1);
+Screen('DrawTexture', w, GreyTexture,[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
 makefix_single(xpos,ypos,Settings.Fixation.Size,Settings.Fixation.ColourWaitForKeyPress,w);
 [FlipStart, StimOnset] = Screen('Flip',w);
 
@@ -185,7 +226,7 @@ Results.StimOnset(mytrialnum) = StimOnset;
       RT = NaN; secs = NaN; keyCode = NaN;
       
       FlushEvents;  RestrictKeysForKbCheck([Settings.Key1 Settings.Key2 EscapeKey]);
-      stop = GetSecs + Settings.Fixation.Duration/1000;
+      stop = GetSecs + Settings.Fixation.DurationGreen/1000;
             while GetSecs < stop
                 [keyIsDown, secs, keyCode] = KbCheck;
                 if keyIsDown              
@@ -194,8 +235,9 @@ Results.StimOnset(mytrialnum) = StimOnset;
                     break; 
                 end
          %      WaitSecs(.01); %It is a good habit not to poll as fast as possible
-       end
+            end
 
+Results.ResponseCounter(mytrialnum) = myresponses;
 Results.ResponseRT(mytrialnum) = RT; Results.ResponseKey{mytrialnum} = KbName(min(find(keyCode)));
 Results.ResponseOnset(mytrialnum) = secs;            
       
@@ -203,34 +245,128 @@ if keyCode(EscapeKey)
    break; % abort program
 end
 
-% Screen('DrawTexture', w, myprimetexture,[], recter(Settings.StimSize, [xpos(Settings.NonDomEye) ypos], [x y]), [], [], 1);
+% if button was pressed, present a stimulus ("effect")
 
-WaitSecs(1);
-       
-% display empty screen with fixation cross
+if keyCode(Settings.Key1) && Settings.Mapping == 0
 Screen('FillRect', w, Settings.Background.Greyvalue, wRect);
-makefix_single(xpos,ypos,Settings.Fixation.Size,Settings.Fixation.Colour,w);  
+Screen('DrawTexture', w, FrameTexture,[], recter(Settings.FrameSize, [xpos ypos], [x y]), [], [], 1);
+
+if Settings.CatchTrial(myresponses)
+Screen('DrawTexture', w, MyTextures{2},[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
+else
+Screen('DrawTexture', w, MyTextures{1},[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
+end
+
+Screen('Flip',w);
+WaitSecs(Settings.StimDuration/1000);
+end
+
+if keyCode(Settings.Key1) && Settings.Mapping == 1
+Screen('FillRect', w, Settings.Background.Greyvalue, wRect);
+Screen('DrawTexture', w, FrameTexture,[], recter(Settings.FrameSize, [xpos ypos], [x y]), [], [], 1);
+
+if Settings.CatchTrial(myresponses)
+Screen('DrawTexture', w, MyTextures{1},[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
+else
+Screen('DrawTexture', w, MyTextures{2},[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
+end
+
+Screen('Flip',w);
+WaitSecs(Settings.StimDuration/1000);
+end
+
+if keyCode(Settings.Key2) && Settings.Mapping == 0
+Screen('FillRect', w, Settings.Background.Greyvalue, wRect);
+Screen('DrawTexture', w, FrameTexture,[], recter(Settings.FrameSize, [xpos ypos], [x y]), [], [], 1);
+
+if Settings.CatchTrial(myresponses)
+Screen('DrawTexture', w, MyTextures{1},[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
+else
+Screen('DrawTexture', w, MyTextures{2},[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
+end
+
+Screen('Flip',w);
+WaitSecs(Settings.StimDuration/1000);
+end
+
+if keyCode(Settings.Key2) && Settings.Mapping == 1
+Screen('FillRect', w, Settings.Background.Greyvalue, wRect);
+Screen('DrawTexture', w, FrameTexture,[], recter(Settings.FrameSize, [xpos ypos], [x y]), [], [], 1);
+
+if Settings.CatchTrial(myresponses)
+Screen('DrawTexture', w, MyTextures{2},[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
+else
+Screen('DrawTexture', w, MyTextures{1},[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
+end
+
+Screen('Flip',w);
+WaitSecs(Settings.StimDuration/1000);
+end
+       
+% display empty screen
+Screen('FillRect', w, Settings.Background.Greyvalue, wRect);
+Screen('DrawTexture', w, FrameTexture,[], recter(Settings.FrameSize, [xpos ypos], [x y]), [], [], 1);
+Screen('DrawTexture', w, GreyTexture,[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
 Screen('Flip',w);
 
-WaitSecs(1);
+% check whether participant catches the catch trial !
+% timeout: ITI
 
-Results.mystimulus(mytrialnum) = mytrialnum;
+% wait for key press
+      RT2 = NaN; secs = NaN; keyCode = NaN;
+      
+      FlushEvents;  RestrictKeysForKbCheck([SpaceKey EscapeKey]);
+      stop = GetSecs + Settings.ITI/1000;
+            while GetSecs < stop
+                [keyIsDown, secs, keyCode] = KbCheck;
+                if keyIsDown              
+                    RT2 = secs - StimOnset;                                 
+                    break; 
+                end
+         %      WaitSecs(.01); %It is a good habit not to poll as fast as possible
+            end
 
+Results.ResponseRT2(mytrialnum) = RT2;            
+            
+if keyCode(EscapeKey)
+   break; % abort program
+end
 
+while GetSecs < stop % loop until ITI timeout
+end
 
+% if there is a catch trial and no RT2 = negative feedback!
+
+if not(isnan(RT))
+if Settings.CatchTrial(myresponses) && isnan(RT2)
+Screen('FillRect', w, Settings.Background.Greyvalue, wRect);
+Screen('DrawTexture', w, FrameTexture,[], recter(Settings.FrameSize, [xpos ypos], [x y]), [], [], 1);
+Screen('DrawTexture', w, GreyTexture,[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
+[nx, ny, bbox] = DrawFormattedText(w, 'FAIL TO CATCH!', x/2 , Settings.Stimuli.CenterXY(2) , Settings.Red);
+Screen('Flip',w);
+WaitSecs(Settings.Feedback.Duration/1000);
+end
+end
+                        
 if myresponses == Settings.numberoftrials break; end; % end of experiment
 
 disp(mytrialnum);
 
 end % TRIAL LOOP
 
-    Screen('FillRect', w, Settings.Background.Greyvalue, wRect);
-    Screen('Flip', w);
+Screen('FillRect', w, Settings.Background.Greyvalue, wRect);
+Screen('DrawTexture', w, FrameTexture,[], recter(Settings.FrameSize, [xpos ypos], [x y]), [], [], 1);
+Screen('DrawTexture', w, GreyTexture,[], recter(Settings.StimSize, [xpos ypos], [x y]), [], [], 1);
+[nx, ny, bbox] = DrawFormattedText(w, 'ENDE', x/2 , Settings.Stimuli.CenterXY(2) , Settings.Writing.Colour);
+Screen('Flip',w);
+
+WaitSecs(1);
 
     % _____________________________________________________________________
 
     Results.ExperimentStop = GetSecs;
-    Results.Instruction = Settings.Instruction;
+    Results.Mapping = Settings.Mapping;
+    Results.CatchTrial = Settings.CatchTrial;
     save([Settings.rootdir '\Results\Results_' num2str(Settings.Vpn) '_' d_t ], 'Results');
     save([Settings.rootdir '\Results\Settings_' num2str(Settings.Vpn) '_' d_t ], 'Settings');
     save([Settings.rootdir '\Results\Workspace_' num2str(Settings.Vpn) '_' d_t ]);
@@ -248,7 +384,8 @@ end % TRIAL LOOP
 catch
 
     Results.ExperimentStop = GetSecs;
-    Results.Instruction = Settings.Instruction;
+    Results.Mapping = Settings.Mapping;
+    Results.CatchTrial = Settings.CatchTrial;
     save([Settings.rootdir '\Results\Results_' num2str(Settings.Vpn) '_' d_t ], 'Results');
     save([Settings.rootdir '\Results\Settings_' num2str(Settings.Vpn) '_' d_t ], 'Settings');
     save([Settings.rootdir '\Results\Workspace_' num2str(Settings.Vpn) '_' d_t ]);
